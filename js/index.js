f8a7624cf5997ea7f9364cb263bf8e72fe6b10fc
Noah Velickovic <noah.velic@hotmail.com>
2025-04-02 09:52:50 +0200

    Summary zu Dashboard.html verschoben

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://join-382e0-default-rtdb.europe-west1.firebasedatabase.app/"
-
-async function loadData() {
-let response = await fetch(BASE_URL + ".json")
-let responseToJson = await response.json()
-console.log(responseToJson.taskList.length);
-
-}
