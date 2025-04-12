cb4bf47d03e28b8b5cd391cf616b1989e5e34cc3
Dora Telekesi <dorothea0929@gmail.com>
2025-04-12 14:39:36 +0200

    add subtask function corrected

--- js/add-task/subTask-add-task.js
+++ js/add-task/subTask-add-task.js
@@ -8,13 +8,24 @@ function changeSubtaskButtons() {
 }
 let subtasks = [];
 function addSubtask() {
+  let subtaskObj = {};
   let inputRef = document.getElementById("subtask");
   if (inputRef.value !== "") {
     let input = inputRef.value;
-    subtasks.push(input);
-    document.getElementById("subtask-list").innerHTML += `<li>${input}</li>`;
+    subtaskObj.status = "undone";
+    subtaskObj.title = input;
+    document.getElementById("subtask-list").innerHTML += `<ul class="subtask-list-item" id="subtask-list-item">
+    <li>${input}</li>
+    <div class="subtask-list-item-btns">
+    <img class="subtask-edit-icons edit" src="./assets/icons/edit.svg" alt="Edit Subtask Icon"/>
+    <div class="subtask-list-item-separator"></div>
+    <img class="subtask-edit-icons delete"  src="./assets/icons/delete.svg" alt="Delete Subtask Icon"/>
+    </div>
+    </ul>`;
     inputRef.value = "";
   }
+  subtasks.push(subtaskObj);
+  console.log(subtasks);
 }
 
 function acceptOrCancelSubtask() {
