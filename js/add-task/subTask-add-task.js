43dd32d720dd26112aad1ee85adecc1599087fb1
Dora Telekesi <dorothea0929@gmail.com>
2025-04-13 18:44:59 +0200

    subtask list edit and delete - on Add Task Page

--- js/add-task/subTask-add-task.js
+++ js/add-task/subTask-add-task.js
@@ -14,18 +14,34 @@ function addSubtask() {
     let input = inputRef.value;
     subtaskObj.status = "undone";
     subtaskObj.title = input;
-    document.getElementById("subtask-list").innerHTML += `<ul class="subtask-list-item" id="subtask-list-item">
-    <li>${input}</li>
-    <div class="subtask-list-item-btns">
-    <img class="subtask-edit-icons edit" src="./assets/icons/edit.svg" alt="Edit Subtask Icon"/>
-    <div class="subtask-list-item-separator"></div>
-    <img class="subtask-edit-icons delete"  src="./assets/icons/delete.svg" alt="Delete Subtask Icon"/>
-    </div>
-    </ul>`;
+    subtasks.push(subtaskObj);
+    // Render only the new subtask
+    const subtaskList = document.getElementById("subtask-list");
+    subtaskList.innerHTML += subtaskListTemplate(subtasks.length - 1);
     inputRef.value = "";
   }
-  subtasks.push(subtaskObj);
   console.log(subtasks);
+}
+
+function subtaskListTemplate(i) {
+  return `<div class="subtask-list-item" id="subtask-list-item-${i}" 
+    onmouseover="showEditLayout(this)" 
+    onmouseout="removeEditLayout(this)">
+    <li id="title-${i}">${subtasks[i].title}</li>
+    <div class="subtask-list-item-btns dp-none">
+      <img class="subtask-edit-icons edit" onclick="editSubtaskItem(${i})" src="./assets/icons/edit.svg" alt="Edit Subtask Icon"/>
+      <div class="subtask-list-item-separator"></div>
+      <img class="subtask-edit-icons delete" onclick="deleteSubtaskItem(${i})" src="./assets/icons/delete.svg" alt="Delete Subtask Icon"/>
+    </div>
+  </div>
+  <div id="input-container-${i}" class="change-input dp-none">
+    <input id="input-${i}" type="text">
+    <div class="subtask-list-item-btns-2">
+      <img class="subtask-edit-icons delete-2" onclick="deleteSubtaskItem(${i})" src="./assets/icons/delete.svg" alt="Edit Subtask Icon"/>
+      <div class="subtask-list-item-separator-2"></div>
+      <img class="subtask-edit-icons accept" onclick="acceptSubtaskItem(${i})" src="./assets/icons/check.svg" alt="Delete Subtask Icon"/>
+    </div>
+  </div>`;
 }
 
 function acceptOrCancelSubtask() {
@@ -37,4 +53,44 @@ function acceptOrCancelSubtask() {
 
 function cancelSubTask() {
   document.getElementById("subtask").value = "";
+}
+
+function showEditLayout(element) {
+  element.querySelector(".subtask-list-item-btns").classList.remove("dp-none");
+}
+function removeEditLayout(element) {
+  element.querySelector(".subtask-list-item-btns").classList.add("dp-none");
+}
+
+function deleteSubtaskItem(index) {
+  // Remove the subtask from the array
+  subtasks.splice(index, 1);
+
+  // Clear the current subtask list in the DOM
+  const subtaskList = document.getElementById("subtask-list");
+  subtaskList.innerHTML = ""; // Reset the list to an empty state
+
+  // Re-render the subtask list using a for loop
+  for (let i = 0; i < subtasks.length; i++) {
+    subtaskList.innerHTML += subtaskListTemplate(i); // Generate and append each subtask's HTML
+  }
+
+  // Log the updated subtasks array for debugging purposes
+  console.log("Updated subtasks:", subtasks);
+}
+
+function editSubtaskItem(id) {
+  document.getElementById(`input-container-${id}`).classList.remove("dp-none");
+  document.getElementById(`subtask-list-item-${id}`).classList.add("dp-none");
+  document.getElementById(`input-${id}`).value = subtasks[id].title;
+}
+
+function acceptSubtaskItem(id) {
+  if (document.getElementById(`input-${id}`).value !== "") {
+    document.getElementById(`input-container-${id}`).classList.add("dp-none");
+    document.getElementById(`subtask-list-item-${id}`).classList.remove("dp-none");
+    subtasks[id].title = document.getElementById(`input-${id}`).value;
+    console.log(document.getElementById(`input-${id}`).value);
+    document.getElementById(`title-${id}`).innerHTML = document.getElementById(`input-${id}`).value;
+  }
 }
