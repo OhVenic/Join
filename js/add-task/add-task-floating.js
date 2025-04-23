fa7430f4a7910a7b9ad19b4fc33ba5133fd35e01
Noah Velickovic <noah.velic@hotmail.com>
2025-04-23 19:57:27 +0200

    merge

--- js/add-task/add-task-floating.js
+++ js/add-task/add-task-floating.js
@@ -19,7 +19,6 @@ function openFloatingAddTask() {
   document.getElementById("overlay").classList.remove("dp-none");
   document.getElementById("add-task-content").classList.remove("dp-none");
   document.getElementById("add-task-content").classList.remove("closing");
-  document.documentElement.style.overflow = "hidden";
 }
 
 /**
